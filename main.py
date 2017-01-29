5d574c4e4302750bb0ca7a09895d17818e6b2efe
JigAma <NathanDuriez@protonmail.com>
2017-01-29 21:11:53 +0100

    Added main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from twitter import * #Import twitter library
+
+#Twitter OAuth
+t = Twitter (
+auth = OAuth(
+    consumer_key='YOUR CONSUMER KEY',
+    consumer_secret='YOUR CONSUMER SECRET',
+    token='YOUR TOKEN',
+    token_secret='YOUR TOKEN SECRET'))
+
+Cp1Ami = 'C\'est pour un ami'
+	
+	
